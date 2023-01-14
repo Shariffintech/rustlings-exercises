8e8b1e29ce25a0e7b4edc4e2387e78703e3073af
Sharif.T <Shariftarver@gmail.com>
2023-01-14 03:14:26 -0500

    move semantics and vecs under my belt

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,13 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let vec0 = Vec::new();
+    let vecClone = vec0.clone();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vecClone);
+
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,14 +2,15 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    let data2 = data.clone();
 
-    string_uppercase(&data);
+    get_char(data2);
+
+    string_uppercase(data);
 }
 
 // Should not take ownership
@@ -18,8 +19,8 @@ fn get_char(data: String) -> char {
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
